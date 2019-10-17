75b3cbd19ae2e5c026be4f683a9b7a41bc1528af
suhas kashyap <suhaskashyap2@gmail.com>
2019-10-17 15:33:47 +0530

    Update models.py

--- Enamour/models.py
+++ Enamour/models.py
@@ -1,3 +1,12 @@
+from django.db import models
+
+
+class payment_stats(models.Model):
+	status = models.CharField(max_length=10)
+	firstname = models.CharField(max_length=10)
+	amount = models.FloatField(max_length=10)
+	txnid = models.CharField(max_length=20)
+	productinfo = models.CharField(max_length=20)
 
 
 
